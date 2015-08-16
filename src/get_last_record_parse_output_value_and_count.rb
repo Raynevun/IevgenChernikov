1210062f7830ab763bcccb6286fbf6dfc7151850
Ievgen Chernikov <ievgenc@Ievgens-MacBook-Pro.local>
2015-08-16 14:28:07 +0100

    puts removed

--- src/get_last_record_parse_output_value_and_count.rb
+++ src/get_last_record_parse_output_value_and_count.rb
@@ -5,7 +5,6 @@ class GetLastRecordParseOutputValueAndCount
   helpers = Helpers.new
   last_beacon_record = LastBeaconRecord.new
   output_value = last_beacon_record.get_output_value
-  puts output_value
   splitted_value = helpers.split_value_char_by_char(output_value)
   counted_value = helpers.count_chars_in_output_value(splitted_value)
   helpers.print_counted_value(counted_value)
